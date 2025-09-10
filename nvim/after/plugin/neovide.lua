0059a2b6f640ffb6e634df9805c09bb5a6024615
Nirvaan Salian <nirvaansaalian@gmail.com>
2025-09-10 16:24:30 +0530

    update: remove cursor animations for snappier response

--- nvim/after/plugin/neovide.lua
+++ nvim/after/plugin/neovide.lua
@@ -23,6 +23,24 @@ if is_neovide then
 
   vim.g.neovide_hide_mouse_when_typing = true
 
+  -- Cursor Animations
+  vim.g.neovide_cursor_animation_length = 0 -- disable animation entirely
+
+  -- vim.g.neovide_cursor_short_animation_length = 0 -- no “typing” micro-movement
+
+  vim.g.neovide_cursor_trail_size = 0 -- remove smear/trailing
+
+  -- vim.g.neovide_cursor_animate_in_insert_mode = false -- jump instantly in insert mode
+
+  -- vim.g.neovide_cursor_animate_command_line = false -- no hop to cmdline
+
+  -- vim.g.neovide_cursor_vfx_mode = "" -- ensure no particle effects
+
+  -- vim.g.neovide_scroll_animation_length = 0 -- remove scroll animation 
+
+  -- vim.g.neovide_position_animation_length = 0  -- remove position animation
+
+
   --vim.keymap.set('n', '<leader>P', '"+gP', { noremap = true, silent = true}) -- Paste normal mode
   --vim.keymap.set('v', '<leader>P', '"+gP', { noremap = true, silent = true}) -- Paste visual mode
   --vim.keymap.set('c', '<leader>P', '<C-R>+', { noremap = true, silent = true}) -- Paste command mode
